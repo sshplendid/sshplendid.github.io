9f24770e1b25779ab55db27feec6a698432f829e
SeHun Shin <sshplendid@gmail.com>
2018-03-24 23:24:32 +0900

    toys project: todo-vue
    toy 프로젝트: todo-vue 추가

---
+++ toys/todo-vue/js/todos/todosApp.js
@@ -0,0 +1,69 @@
+(function(exports) {
+
+  'use strct';
+
+  var filters = {
+    all: function(todos) {
+      return todos;
+    },
+    incompleted: function(todos) {
+      return todos.filter(todo => {
+        return !todo.completion;
+      });
+    },
+    completed: function(todos) {
+      return todos.filter(todo => {
+        return todo.completion;
+      });
+    }
+  }
+
+  exports.todosApp = new Vue({
+    el: '#todosApp',
+    data: {
+      input: null,
+      todos: todoStorage.fetch() ,
+      filterFlag: 'all',
+      currentUser: userStorage.getCurrentUser()
+    },
+    methods: {
+      addTodo: function() {
+        this.input = this.input && this.input.trim();
+        if (!this.input) {
+          return;
+        }
+
+        var todo = {id:this.todos.length, message:this.input, completion: false};
+        this.todos.push(todo);
+        this.input = '';
+      },
+      removeTodo: function(_id) {
+        todosApp.todos.splice(_id, 1);
+      },
+      toggleCompletion: function(_id) {
+        todosApp.todos[_id].completion = !todosApp.todos[_id].completion;
+      },
+      signOut: function() {
+        userStorage.flushCurrentUser();
+        window.location.href = '/toys/todo-vue/';
+      },
+      signInPage: function() {
+        if(userStorage.getCurrentUser().name)
+          return;
+        window.location.href = window.location.pathname + '/login';
+      }
+    },
+    computed: {
+      filteredTodos: function() {
+        return filters[this.filterFlag](this.todos);
+      }
+    },
+    watch: {
+      todos: {
+        deep: true,
+        handler: todoStorage.save
+      }
+    }
+  });
+
+})(window);
---
+++ toys/todo-vue/js/todos/todosRouter.js
@@ -0,0 +1,28 @@
+(function (app, Router) {
+
+	'use strict';
+
+	var routes = {
+		'/all': function() {
+			todosApp.filterFlag = 'all';
+		},
+		'/incompleted': function() {
+			todosApp.filterFlag = 'incompleted';
+		},
+		'/completed': function() {
+			todosApp.filterFlag = 'completed';
+		}
+	};
+
+	var options = {
+		notfound: function() {
+			window.location.hash = '';
+			todosApp.filterFlag = 'all';
+		}
+		// ,html5history: true
+	}
+	var router = new Router(routes);
+	router.configure(options);
+	router.init();
+
+})(todosApp, Router);
